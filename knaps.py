e53cf08579d1f8151234b65047351f48d5052d12
Shintaalya <111713790+Shintaalya@users.noreply.github.com>
2023-06-13 20:55:31 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -40,85 +40,8 @@ with data_set_description:
     st.write("Link github Aplikasi : https://github.com/Shintaalya/repo")
 
 with modeling:
-    training, test = train_test_split(scaled_features,test_size=0.2, random_state=1)#Nilai X training dan Nilai X testing
-    training_label, test_label = train_test_split(y, test_size=0.2, random_state=1)#Nilai Y training dan Nilai Y testing
-    with st.form("modeling"):
-        st.subheader('Modeling')
-        st.write("Pilihlah model yang akan dilakukan pengecekkan akurasi:")
-        naive = st.checkbox('Gaussian Naive Bayes')
-        k_nn = st.checkbox('K-Nearest Neighboor')
-        destree = st.checkbox('Decission Tree')
-        submitted = st.form_submit_button("Submit")
-
-        # NB
-        GaussianNB(priors=None)
-
-        # Fitting Naive Bayes Classification to the Training set with linear kernel
-        gaussian = GaussianNB()
-        gaussian = gaussian.fit(training, training_label)
-
-        # Predicting the Test set results
-        y_pred = gaussian.predict(test)
+    st.write("masih kosong")
     
-        y_compare = np.vstack((test_label,y_pred)).T
-        gaussian.predict_proba(test)
-        gaussian_akurasi = round(100 * accuracy_score(test_label, y_pred))
-        # akurasi = 10
-
-        #Gaussian Naive Bayes
-        # gaussian = GaussianNB()
-        # gaussian = gaussian.fit(training, training_label)
-
-        # probas = gaussian.predict_proba(test)
-        # probas = probas[:,1]
-        # probas = probas.round()
-
-        # gaussian_akurasi = round(100 * accuracy_score(test_label,probas))
-
-        #KNN
-        K=10
-        knn=KNeighborsClassifier(n_neighbors=K)
-        knn.fit(training,training_label)
-        knn_predict=knn.predict(test)
-
-        knn_akurasi = round(100 * accuracy_score(test_label,knn_predict))
-
-        #Decission Tree
-        dt = DecisionTreeClassifier()
-        dt.fit(training, training_label)
-        # prediction
-        dt_pred = dt.predict(test)
-        #Accuracy
-        dt_akurasi = round(100 * accuracy_score(test_label,dt_pred))
-
-        if submitted :
-            if naive :
-                st.write('Model Naive Bayes accuracy score: {0:0.2f}'. format(gaussian_akurasi))
-            if k_nn :
-                st.write("Model KNN accuracy score : {0:0.2f}" . format(knn_akurasi))
-            if destree :
-                st.write("Model Decision Tree accuracy score : {0:0.2f}" . format(dt_akurasi))
-        
-        grafik = st.form_submit_button("Grafik akurasi semua model")
-        if grafik:
-            data = pd.DataFrame({
-                'Akurasi' : [gaussian_akurasi, knn_akurasi, dt_akurasi],
-                'Model' : ['Gaussian Naive Bayes', 'K-NN', 'Decission Tree'],
-            })
-
-            chart = (
-                alt.Chart(data)
-                .mark_bar()
-                .encode(
-                    alt.X("Akurasi"),
-                    alt.Y("Model"),
-                    alt.Color("Akurasi"),
-                    alt.Tooltip(["Akurasi", "Model"]),
-                )
-                .interactive()
-            )
-            st.altair_chart(chart,use_container_width=True)
-  
 with implementation:
     with st.form("my_form"):
         st.subheader("Implementasi")
