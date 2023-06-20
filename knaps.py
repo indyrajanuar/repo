89cb6c62e2cec231c0cd6ad76c0e7e318e04c39f
Shintaalya <111713790+Shintaalya@users.noreply.github.com>
2023-06-20 22:02:47 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -108,8 +108,8 @@ def main():
     st.title('Prediksi Harga Rumah')
 
     # Input form
-    input_data_1 = st.text_input('Luas Tanah', '1.0')
-    input_data_2 = st.text_input('Luas Bangunan', '2.0')
+    input_data_1 = st.text_input('Luas Tanah', '')
+    input_data_2 = st.text_input('Luas Bangunan', '')
 
     # Check if input values are numeric
     if not input_data_1.isnumeric() or not input_data_2.isnumeric():
