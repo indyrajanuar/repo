21c629dbc7f6afac7a4de9227dfab2e7f5ba931b
Shintaalya <111713790+Shintaalya@users.noreply.github.com>
2023-06-20 22:55:42 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -104,6 +104,7 @@ def denormalize_data(data):
     return denormalized_data
 
 # Streamlit app code
+    def mainn():
     st.title('Prediksi Harga Rumah')
 
     # Input form
@@ -130,3 +131,5 @@ def denormalize_data(data):
     # Display the prediction
     st.subheader('Hasil Prediksi')
     st.write(prediction[0])
+if __name__ == "__main__":
+    mainn()
