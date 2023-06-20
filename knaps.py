d4070e0f410a17da25983d5e7cb0f72256c675fd
Shintaalya <111713790+Shintaalya@users.noreply.github.com>
2023-06-20 22:33:25 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -133,9 +133,5 @@ def main():
     st.subheader('Hasil Prediksi')
     st.write(prediction[0])
 
-if __name__ == "__main__":
-    main()
-
-
 if choose=='Help':
     st.markdown('<h1 style = "text-align: center;"> Panduan : </h1><ol type = "1" style = "text-align: justify; background-color: #00FFFF; padding: 30px; border-radius: 20px;"><li><i><b>Cara View Dataset</b></i> <ol type = "a"><li>Masuk ke sistem</li><li>Pilih menu dataset</li></ol></li><li><i><b>Cara Prediksi Harga</b></i> <ol type = "a"><li>Pilih menu predict</li><li>Pilih LT dan LB</li><li>Klik tombol prediksi</li></ol></li></ol>', unsafe_allow_html = True)
