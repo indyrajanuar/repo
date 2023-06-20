dc0cebd5f0e7ffdf059b9c1022dbdad017f9c9bb
Shintaalya <111713790+Shintaalya@users.noreply.github.com>
2023-06-20 23:04:02 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -22,7 +22,7 @@ with st.sidebar:
             "nav-link-selected": {"background-color": "#00FFFF"},
         }
         )
-elif choose=='Home':
+if choose=='Home':
     st.markdown('<h1 style = "text-align: center;"> Prediksi Harga Rumah</h1>', unsafe_allow_html = True)
     logo = Image.open('makam1.jpg')
 
