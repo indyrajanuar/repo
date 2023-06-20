23e9976792bb0fb30a3543b31a631fdf23b1d4da
Shintaalya <111713790+Shintaalya@users.noreply.github.com>
2023-06-20 22:01:19 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -65,8 +65,8 @@ if choose=='Prepocessing':
     logo = Image.open('dataset3.png')
     st.image(logo, caption='')
     
-if choose == 'Predict':
-    st.markdown('<h1 style="text-align: center;">Prediksi Harga Rumah</h1>', unsafe_allow_html=True)
+if choose=='Predict':
+    st.markdown('<h1 style = "text-align: center;"> Prediksi Harga Rumah</h1>', unsafe_allow_html = True)
     logo = Image.open('eror.png')
     st.image(logo, caption='')
     import pickle
@@ -76,9 +76,9 @@ if choose == 'Predict':
     url = 'https://raw.githubusercontent.com/Shintaalya/repo/main/model.pkl'
     filename = 'model.pkl'  # Nama file yang akan disimpan secara sementara
     urllib.request.urlretrieve(url, filename)
-
+    
     # Load the model
-    with open('model.pkl', 'rb') as file:
+    with open('model.pkl','rb') as file:
         model_data = pickle.load(file)
         model = model_data['model']
         X_train_expanded = model_data['X_train_expanded']
@@ -87,8 +87,8 @@ if choose == 'Predict':
         best_X_train = model_data['best_X_train']
         best_y_train = model_data['best_y_train']
 
-
-    def normalize_input_data(data):
+# Function to normalize input data
+def normalize_input_data(data):
     normalized_data = (data - np.mean(best_X_train, axis=0)) / np.std(best_X_train, axis=0)
     return normalized_data
 
@@ -111,44 +111,28 @@ def main():
     input_data_1 = st.text_input('Luas Tanah', '1.0')
     input_data_2 = st.text_input('Luas Bangunan', '2.0')
 
-    try:
-        # Convert input values to float
-        input_feature_1 = float(input_data_1)
-        input_feature_2 = float(input_data_2)
-
-        # Normalize and expand input features
-        input_features = np.array([[input_feature_1, input_feature_2]])
-        expanded_input = expand_input_features(input_features)
-
-        # Perform prediction
-        normalized_prediction = model.predict(expanded_input)
-        prediction = denormalize_data(normalized_prediction)
-
-        # Display the prediction
-        st.subheader('Prediction')
-        st.write(prediction[0])
-    except ValueError:
+    # Check if input values are numeric
+    if not input_data_1.isnumeric() or not input_data_2.isnumeric():
         st.error('Please enter numeric values for the input features.')
+        return
+
+    # Convert input values to float
+    input_feature_1 = float(input_data_1)
+    input_feature_2 = float(input_data_2)
+
+    # Normalize and expand input features
+    input_features = np.array([[input_feature_1, input_feature_2]])
+    expanded_input = expand_input_features(input_features)
+
+    # Perform prediction
+    normalized_prediction = model.predict(expanded_input)
+    prediction = denormalize_data(normalized_prediction)
+
+    # Display the prediction
+    st.subheader('Prediction')
+    st.write(prediction[0])
 
 if __name__ == "__main__":
-    choose = 'Predict'
-    st.markdown('<h1 style="text-align: center;">Prediksi Harga Rumah</h1>', unsafe_allow_html=True)
-    logo = Image.open('eror.png')
-    st.image(logo, caption='')
-    url = 'https://raw.githubusercontent.com/Shintaalya/repo/main/model.pkl'
-    filename = 'model.pkl'
-    urllib.request.urlretrieve(url, filename)
-
-    # Load the model
-    with open('model.pkl', 'rb') as file:
-        model_data = pickle.load(file)
-        model = model_data['model']
-        X_train_expanded = model_data['X_train_expanded']
-        y_train_mean = model_data['y_train_mean']
-        y_train_std = model_data['y_train_std']
-        best_X_train = model_data['best_X_train']
-        best_y_train = model_data['best_y_train']
-
     main()
 
 
