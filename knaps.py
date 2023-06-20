083307c70362567b72e0edbc7d1aee3150c735e7
Shintaalya <111713790+Shintaalya@users.noreply.github.com>
2023-06-20 19:20:26 +0700

    Update knaps.py

--- knaps.py
+++ knaps.py
@@ -95,7 +95,7 @@ if choose=='Predict':
 
         hasil = int(y_pred[0])
 
-        st.write('Prediksi Harga dengan LT:', LT, 'dan LB:', LB, 'adalah', hasil, 'pengunjung')
+        st.write('Prediksi Harga dengan LT:', LT, 'dan LB:', LB, 'adalah', hasil)
 
 if choose=='Help':
     st.markdown('<h1 style = "text-align: center;"> Panduan : </h1><ol type = "1" style = "text-align: justify; background-color: #00FFFF; padding: 30px; border-radius: 20px;"><li><i><b>Cara View Dataset</b></i> <ol type = "a"><li>Masuk ke sistem</li><li>Pilih menu dataset</li></ol></li><li><i><b>Cara Prediksi Harga</b></i> <ol type = "a"><li>Pilih menu predict</li><li>Pilih bulan</li><li>Klik tombol prediksi</li></ol></li></ol>', unsafe_allow_html = True)
